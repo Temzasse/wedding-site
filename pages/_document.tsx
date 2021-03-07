04abc1e7ac68f26678a5ca221d61c015d5978a13
Teemu Taskula <themetor@gmail.com>
2021-03-07 18:58:17 +0200

    Add robots block meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,9 @@ export default class Document extends NextDocument {
     return (
       <Html lang="fi">
         <Head>
+          {/* Block robots */}
+          <meta name="robots" content="noindex" />
+
           <link rel="icon" href="/favicon.ico" />
           <link rel="icon" type="image/png" href="/favicon_32x32.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -20,6 +23,7 @@ export default class Document extends NextDocument {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&family=Playfair+Display:wght@400;500&display=swap"
             rel="stylesheet"
           />
+
           <style id="stitches" dangerouslySetInnerHTML={{ __html: styles }} />
         </Head>
         <body>
