984f3c49a4d763f95e2006be863e83061dfea2a0
Teemu Taskula <themetor@gmail.com>
2021-11-26 21:25:56 +0200

    Cleanup

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -117,7 +117,6 @@ export default function QuizForm() {
                 <ArrowIcon>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
